49e3a0ad9187dc7499667d4ba62047589d5648d0
Ronald Silva <ronald.creative@gmail.com>
2021-09-09 17:58:46 -0300

    add static charts with apexcharts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutsChart from "components/DonutsChart";
 import Footer from "components/Footer/Index";
 import NavBar from "components/NavBar";
 
@@ -8,10 +10,31 @@ function App() {
   return (
     <>
       <NavBar />
-    <div className='container'>     
-      <h1>NavBar</h1>
 
-      <DataTable />
+    <div className='container'>     
+      <h1 className='text-primary py-3'>User Dashboard</h1>
+
+      <div className='row px-3'>
+        <div className='col-sm-6'>
+          <h5  className='text-secondary text-center' >Taxa de sucesso dos vendedores (%) </h5>
+
+        <BarChart />        
+        </div>
+
+        <div className='col-sm-6'>
+          <h5 className='text-secondary text-center' >Taxa de sucesso dos vendedores (%) </h5>
+
+        <DonutsChart />        
+        </div>
+
+        
+     
+      </div>
+
+      <div className='py3'>
+        <h2 className='text-primary'>Todas as vendas</h2>
+      </div>
+      <DataTable />      
     </div>
     
     <Footer />
